961e91fecb073650d026a1a10fcc81376439cf9f
kerryl02 <sodakerrylgmail.com>
2024-08-01 23:15:37 +0200

    TTEST

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Logo from "./Logo"
+
+const Header = () => {
+  return (
+    <>
+        <div className="navbar_component flex items-center border-b">
+                <Logo />
+        </div>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+  return (
+    <>
+        <a href="#" className="navbar_logo-link w-nav-brand">
+          <div className="hb-logo_componement">
+            <div className="logogmark"><img src="src/assets/logomark.svg" alt="hexweb logomark" /></div>
+          </div>
+        </a>
+    </>
+  )
+}
+
+export default Logo
