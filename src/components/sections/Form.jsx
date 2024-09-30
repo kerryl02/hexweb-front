ccda761319822eacdae0565d811c075c08ca24e2
Kerryl02 <b.soda973@gmail.com>
2024-09-30 15:43:13 +0200

    affichage bug

--- src/components/sections/Form.jsx
+++ src/components/sections/Form.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import axios from "axios";
 import Section from "./Section";
@@ -26,8 +25,9 @@ const Form = () => {
     firstname: "",
     email: "",
     phone: "",
-    message: ""
+    message: "",
   });
+  const [errors, setErrors] = useState([]);
   const [status, setStatus] = useState("");
 
   const handleChange = (e) => {
@@ -39,24 +39,37 @@ const Form = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setStatus("");
 
     try {
-      setStatus("Envoi en cours...");
-      
-      const response = await axios.post("http://localhost:5000/api/contact", formData);  // Assure-toi de mettre l'URL de ton backend
+      const response = await axios.post(
+        "http://localhost:5000/api/contact",
+        formData
+      ); // Assure-toi de mettre l'URL de ton backend
       if (response.status === 200) {
         setStatus("Message envoyé avec succès !");
-        setFormData({ name: "", firstname: "", email: "", phone: "", message: "" });
+        setErrors([]);
+        setFormData({
+          name: "",
+          firstname: "",
+          email: "",
+          phone: "",
+          message: "",
+        });
       }
     } catch (error) {
-      setStatus("Erreur lors de l'envoi. Veuillez réessayer.");
+      if (error.response && error.response.data.errors) {
+        setErrors(error.response.data.errors);
+      } else {
+        setErrors([{ msg: "Une erreur est survenue." }]);
+      }
     }
   };
 
   return (
     <div className="bg-gradient-to-br from-cyan-100 to-blue-100">
       <Section>
-        <div className="w-full p-4 mx-auto bg-[#f5f5f5] rounded-none shadow-2xl md:rounded-2xl md:p-8 dark:bg-black">
+        <div className="w-full p-4 mx-auto bg-[#f5f5f5] rounded-none shadow-2xl md:rounded-2xl md:p-8 dark:bg-black bg-white">
           <div>
             <h2 className="text-black text-center text-2xl lg:text-[2rem] font-bold font-kallisto">
               Discutons de Votre Projet Digital !
@@ -65,17 +78,23 @@ const Form = () => {
           <div className="flex flex-col gap-8 pt-12 md:flex-row">
             <div className="flex flex-row md:flex-col md:h-[200px] items-center justify-around w-full text-xl h-[70px]">
               <div className="">
-                <p className="text-lg font-medium text-center uppercase md:text-xl">Par email</p>
+                <p className="text-lg font-medium text-center uppercase md:text-xl">
+                  Par email
+                </p>
                 <p className="text-lg text-center">contact@hexweb.fr</p>
               </div>
               <div>
                 <p className="text-lg font-medium text-center uppercase md:text-xl">
                   par le formulaire
                 </p>
-                <p className="text-lg text-center">description du temp de réponse </p>
+                <p className="text-lg text-center">
+                  description du temp de réponse{" "}
+                </p>
               </div>
               <div>
-                <p className="text-lg font-medium text-center uppercase md:text-xl">Horaires</p>
+                <p className="text-lg font-medium text-center uppercase md:text-xl">
+                  Horaires
+                </p>
                 <p className="text-lg text-center ">Lundi-Vendredi, 8h-17h </p>
               </div>
             </div>
@@ -106,7 +125,6 @@ const Form = () => {
                       <input
                         id="firstname"
                         value={formData.firstname}
-                        
                         placeholder=" "
                         onChange={handleChange}
                         type="text"
@@ -127,7 +145,6 @@ const Form = () => {
                     <input
                       id="email"
                       value={formData.email}
-                     
                       placeholder=" "
                       onChange={handleChange}
                       type="email"
@@ -165,7 +182,6 @@ const Form = () => {
                   <div className="relative">
                     <textarea
                       id="message"
-                      
                       placeholder=" "
                       value={formData.message}
                       onChange={handleChange}
@@ -190,7 +206,16 @@ const Form = () => {
 
                 <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent mt-8 h-[1px] w-full" />
               </form>
-              <p>{status}</p>
+              {errors.length > 0 && (
+                <div className="error-messages">
+                  <ul>
+                    {errors.map((error, index) => (
+                      <li key={index}>{error.msg}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {status && <p>{status}</p>}
             </div>
           </div>
         </div>
